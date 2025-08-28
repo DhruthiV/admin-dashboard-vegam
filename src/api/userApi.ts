576dc4fc95239cbfa7a2932f75d165d841baa222
DhruthiV <dhruthivenkatesh.pmcs.369@gmail.com>
2025-08-28 18:32:49 +0530

    main app setup with tanstack and connected to userapi

---
+++ src/api/userApi.ts
@@ -0,0 +1,39 @@
+import type { User } from "../mocks/types";
+
+//api calls to user data get and patch
+//get from users
+export async function getUsers(params: {
+  page: number;
+  pageSize: number;
+  sorting?: unknown;
+  query?: string;
+}): Promise<{ users: User[]; totalCount: number }> {
+  const { page, pageSize, query } = params;
+  const queryParams = new URLSearchParams({
+    page: String(page),
+    pageSize: String(pageSize),
+    query: query || "",
+  }).toString();
+  const response = await fetch(`/api/users?${queryParams}`);
+  if (!response.ok) {
+    throw new Error("Failed to fetch users");
+  }
+  const result = await response.json();
+  return { users: result.users, totalCount: result.totalCount };
+}
+
+//patch to users
+export const updateUserStatus = async (
+  userId: string,
+  status: string
+): Promise<User> => {
+  const response = await fetch(`/api/users/${userId}`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ status }),
+  });
+  if (!response.ok) {
+    throw new Error("Failed to toggle user status");
+  }
+  return response.json();
+};
